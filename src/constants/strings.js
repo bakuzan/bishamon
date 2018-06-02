24c7369c4a3a369ec2229dd9a5a65ba7cd9f498d
bakuzan <imannoyed4days@hotmail.co.uk>
2018-06-02 21:59:40 +0100

    implemented themes inspired by bishamons various attires.

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,6 +1,9 @@
 const themes = new Map([
   ['default', { text: 'Classic', value: 'default' }],
-  ['theme-two', { text: 'Knight', value: 'theme-two' }]
+  ['theme-two', { text: 'Suit', value: 'theme-two' }],
+  ['theme-three', { text: 'Knight', value: 'theme-three' }],
+  ['theme-four', { text: 'Warlord', value: 'theme-four' }],
+  ['theme-five', { text: 'Kimono', value: 'theme-five' }]
 ]);
 
 export default Object.freeze({
